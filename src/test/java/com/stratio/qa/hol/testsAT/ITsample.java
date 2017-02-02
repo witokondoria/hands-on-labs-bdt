c8ed484c1ef54824158e035366c561cb3e2c90ac
adoblas <adoblas@stratio.com>
2017-02-02 11:49:40 +0100

    fix indentation

--- src/test/java/com/stratio/qa/hol/testsAT/ITsample.java
+++ src/test/java/com/stratio/qa/hol/testsAT/ITsample.java
@@ -1,8 +1,8 @@
 package com.stratio.qa.hol.testsAT;
-        import com.stratio.qa.cucumber.testng.CucumberRunner;
-        import com.stratio.qa.hol.utils.BaseTest;
-        import cucumber.api.CucumberOptions;
-        import org.testng.annotations.Test;
+import com.stratio.qa.cucumber.testng.CucumberRunner;
+import com.stratio.qa.hol.utils.BaseTest;
+import cucumber.api.CucumberOptions;
+import org.testng.annotations.Test;
 
 @CucumberOptions(features = { "src/test/resources/features/sample.feature" })
 public class ITsample extends BaseTest {
